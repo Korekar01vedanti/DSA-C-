e0ff9fb92b038178d23656084a8bdb3fc963321f
Korekar01vedanti <90140384+Korekar01vedanti@users.noreply.github.com>
2021-09-05 22:53:24 +0530

    Add files via upload

---
+++ 9.GradingStudents.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	float mark,sum=0,avg;
+	cout<<"Enter marks obtained in 5 subjects :"<<endl;
+	for(i=0;i<5;i++)
+	{
+		cin>>mark;
+		sum=sum+mark;
+	}
+	avg=sum/5;
+	cout<<"\nGrade = ";
+	if(avg>=91 && avg<=100)
+    {
+    	cout<<"A1";
+	}
+	else
+	{
+		if(avg>=81 && avg<=91)
+        {
+    	    cout<<"A2";
+	    }
+	    else
+	    {
+		    if(avg>=71 && avg<=81)
+            {
+    	        cout<<"B1";
+	        }
+         	else
+	        {
+		        if(avg>=61 && avg<=71)
+                {
+    	            cout<<"B2";
+	            }
+	            else
+	            {
+		            if(avg>=51 && avg<=61)
+                    {
+    	                cout<<"C1";
+	                }
+	                else
+	                {
+		                if(avg>=41 && avg<=51)
+                        {
+                        	cout<<"C2";
+                    	}
+                    	else
+	                    {
+		                    if(avg>=33 && avg<=41)
+                            {
+    	                        cout<<"D";
+	                        }
+	                        else
+	                        {
+		                        cout<<"Failed";
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	    }
+	}
+}
